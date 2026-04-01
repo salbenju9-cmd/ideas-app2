06aff1505e9a50cf7903eeacd82e5fdcae53bfea
salbenju9-cmd <salbenju9@gmail.com>
2026-04-01 22:25:49 +0200

    Add files via upload

---
+++ service-worker-ideas-app.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'gestor-ideas-iphone-app-v2';
+const URLS_TO_CACHE = [
+  './ideas_iphone_app.html',
+  './manifest_ideas_app.webmanifest',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve()))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const networkFetch = fetch(event.request).then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone)).catch(() => {});
+        return response;
+      }).catch(() => cached);
+      return cached || networkFetch;
+    })
+  );
+});
